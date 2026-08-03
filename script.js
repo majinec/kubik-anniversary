32a316461fb5db967ff8d32341912a14f4fa4121
majinec <simswag78@gmail.com>
2026-08-03 17:39:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Májinka & Kubík Anniversary Edition 🎮❤️");
 
 
-// CURSOR
+// CUSTOM CURSOR
 
 const cursor = document.querySelector(".custom-cursor");
 
@@ -24,6 +24,7 @@ const bootBackButton = document.getElementById("bootBackButton");
 
 
 
+
 // SCENES
 
 const startScene = document.getElementById("startScene");
@@ -33,8 +34,212 @@ const gtaScene = document.getElementById("gtaScene");
 
 
 
+
 // VIDEO
 
+const gtaVideo = document.getElementById("gtaVideo");
+
+
+
+
+
+
+
+
+// START → SAVE
+
+startButton.addEventListener("click", ()=>{
+
+
+    console.log("Loading save data...");
+
+
+    startScene.classList.remove("active");
+
+
+    setTimeout(()=>{
+
+
+        saveScene.classList.add("active");
+
+
+    },800);
+
+
+});
+
+
+
+
+
+
+
+
+// SAVE → START
+
+backButton.addEventListener("click", ()=>{
+
+
+    saveScene.classList.remove("active");
+
+
+    setTimeout(()=>{
+
+
+        startScene.classList.add("active");
+
+
+    },500);
+
+
+});
+
+
+
+
+
+
+
+
+// SAVE → PS5 BOOT
+
+continueButton.addEventListener("click", ()=>{
+
+
+    console.log("Starting PS5 Boot...");
+
+
+    saveScene.classList.remove("active");
+
+
+    setTimeout(()=>{
+
+
+        bootScene.classList.add("active");
+
+
+        startBootSequence();
+
+
+    },800);
+
+
+
+});
+
+
+
+
+
+
+
+
+// PS5 BOOT SEQUENCE
+
+function startBootSequence(){
+
+
+    console.log("PS5 BOOT ACTIVE");
+
+
+
+    setTimeout(()=>{
+
+
+        console.log("PS5 BOOT FINISHED");
+
+
+        launchGTA();
+
+
+
+    },15000);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// LAUNCH GTA
+
+function launchGTA(){
+
+
+    console.log("LAUNCHING GTA V");
+
+
+
+    bootScene.classList.remove("active");
+
+
+
+    setTimeout(()=>{
+
+
+        gtaScene.classList.add("active");
+
+
+
+        gtaVideo.currentTime = 0;
+
+
+
+        gtaVideo.play()
+        .then(()=>{
+
+
+            console.log("GTA VIDEO PLAYING");
+
+
+        })
+        .catch((error)=>{
+
+
+            console.log("VIDEO ERROR:", error);
+
+
+        });
+
+
+
+    },1000);
+
+
+
+}
+
+
+
+
+
+
+
+
+// GO BACK FROM PS5
+
+bootBackButton.addEventListener("click", ()=>{
+
+
+    bootScene.classList.remove("active");
+
+
+    setTimeout(()=>{
+
+
+        saveScene.classList.add("active");
+
+
+    },500);
+
+
+});
 const gtaVideo = document.getElementById("gtaVideo");
 
 
