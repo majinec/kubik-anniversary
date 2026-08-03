2e9ae1a621949fa94912ee0ed3cc539a1d581e20
majinec <simswag78@gmail.com>
2026-08-03 14:53:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("CO-OP STORY LOADED ❤️");
+
+setTimeout(() => {
+
+
+    const gta = document.getElementById("gtaScreen");
+
+
+    gta.style.display="block";
+
+
+    setTimeout(()=>{
+
+        gta.classList.add("gta-active");
+
+
+        document.getElementById("gtaVideo").play();
+
+
+    },4000);
+
+
+
+},15000);
