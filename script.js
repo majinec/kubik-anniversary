a176e87d4083642832a4cd75bfd100d65a6634d4
majinec <simswag78@gmail.com>
2026-08-05 23:57:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1406,39 +1406,3 @@ updateChapters();
 
 
 }
-// =================================
-// CHAPTER IV SYSTEM
-// =================================
-
-
-const chapter4Card = document.querySelector(
-    '[data-chapter="chapter4"]'
-);
-
-
-const chapter4IntroScene = document.getElementById(
-    "chapter4IntroScene"
-);
-
-
-
-if(chapter4Card){
-
-
-    chapter4Card.onclick = ()=>{
-
-
-        console.log(
-            "Opening Chapter IV"
-        );
-
-
-        openScene(
-            chapter4IntroScene
-        );
-
-
-    };
-
-
-}
